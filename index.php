fab139d4b8013f785882d8af057f0566339dd8ab
Alessandro <alessandro.corneli11@mail.com>
2020-02-20 15:53:21 +0100

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!-- Creare una variabile con un paragrafo di testo.
+Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la badword passata in GET con tre *. (edited)
+
+cartella repo php-badwords -->
+<?php
+  
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p><?php  ?></p>
+  </body>
+</html>
